90ce670c2e143f52364a6babee2384aab8c1735c
ThomasJilge <thomasjilge@gmx.de>
2024-08-17 08:45:21 +0200

    @Input v2

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -15,14 +15,7 @@ export class FruitlistComponent {
   // fontColorBad = 'red';
 
 fruitlist = [
-  {
-    name: "Apfel",
-    img:"apple.png",
-    description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
-    genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
-    stars: 2.3,
-    reviews:[],
-  },
+  
   {
     name: "Banane",
     img:"banana.png",
